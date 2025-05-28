cf189dc2278593d8d093e63c647f7f81b7bfa601
Shubham Ojha <143591512+shubhamojha98@users.noreply.github.com>
2025-05-28 21:40:49 +0530

    Initial commit

---
+++ app/components/Logo.tsx
@@ -0,0 +1,19 @@
+// import darkLogo from "@/assets/logos/dark.svg";
+// import logo from "@/assets/logos/main.svg";
+// import Image from "next/image";
+
+// export function Logo() {
+//   return (
+
+
+//       <Image
+//         src={darkLogo}
+//         fill
+//         className="hidden dark:block"
+//         alt="NextAdmin logo"
+//         role="presentation"
+//         quality={100}
+//       />
+//     </div>
+//   );
+// }
